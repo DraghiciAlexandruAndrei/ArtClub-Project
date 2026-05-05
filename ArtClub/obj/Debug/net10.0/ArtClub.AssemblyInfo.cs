ff179727ced9e6c56376df50eece893b9dad05f8
DraghciAlexandruAndrei <draghici.alexandru.r5q@student.ucv.ro>
2026-05-05 16:43:08 +0300

    actualizat si merge totul

--- ArtClub/obj/Debug/net10.0/ArtClub.AssemblyInfo.cs
+++ ArtClub/obj/Debug/net10.0/ArtClub.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArtClub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c21a0e705d971e9ac0e392356422b219f5f7566")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+267830e8460fa6e4fea32a93ac58c98c661fa964")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArtClub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArtClub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
