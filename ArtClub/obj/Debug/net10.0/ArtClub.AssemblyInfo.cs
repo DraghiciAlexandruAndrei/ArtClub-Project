3da4f46ac2e01437c4a975a6870e4f796150c24d
DraghciAlexandruAndrei <draghici.alexandru.r5q@student.ucv.ro>
2026-05-07 10:32:12 +0300

    v0.10

--- ArtClub/obj/Debug/net10.0/ArtClub.AssemblyInfo.cs
+++ ArtClub/obj/Debug/net10.0/ArtClub.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArtClub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+288bc5bf64866356abc48ea4c556f943eb6f2850")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32d667c9711cd3e9b0f44d6b46fe5d879552ba89")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArtClub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArtClub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
